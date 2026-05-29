08da732641f829d5d131d1bed2cfe5d24e501af3
ShadowTheHedgehog <Christianthehedgehogtessit@gmail.com>
2026-05-29 19:45:31 -0400

    glyph fix

--- src/core/glyphs/auto-glyph-processor.js
+++ src/core/glyphs/auto-glyph-processor.js
@@ -238,14 +238,14 @@ function getGlyphLevelSources() {
     : Currency.eternityPoints.value;
   eternityPoints = Decimal.max(player.records.thisReality.maxEP, eternityPoints);
   const epCoeff = 0.016;
-  const epBase = Decimal.pow(Decimal.max(1, eternityPoints.add(1).pLog10()), 0.5).times(epCoeff).toNumber();
+  const epBase = Decimal.min(Decimal.pow(Decimal.max(1, eternityPoints.add(1).pLog10()), 0.5).times(epCoeff).toNumber(),DC.E300);
   const replPow = 0.4 + getAdjustedGlyphEffect("replicationglyphlevel");
   const replCoeff = 0.025;
-  const replBase = Decimal.pow(Decimal.max(1, player.records.thisReality.maxReplicanti.add(1).log10()), replPow).times(replCoeff).toNumber();
+  const replBase = Decimal.min(Decimal.pow(Decimal.max(1, player.records.thisReality.maxReplicanti.add(1).log10()), replPow).times(replCoeff).toNumber(),DC.E300);
   const dtPow = 1.3 + getAdjustedGlyphEffect("realityDTglyph");
   const dtCoeff = 0.025;
-  const dtBase = Decimal.pow(Decimal.max(1, player.records.thisReality.maxDT.add(1).pLog10()), dtPow).times(dtCoeff).toNumber();
-  const eterBase = Effects.max(1, RealityUpgrade(18));
+  const dtBase = Decimal.min(Decimal.pow(Decimal.max(1, player.records.thisReality.maxDT.add(1).pLog10()), dtPow).times(dtCoeff).toNumber(),DC.E300);
+  const eterBase = Decimal.min(Effects.max(1, RealityUpgrade(18)),DC.E300);
   return {
     ep: {
       name: "EP",
