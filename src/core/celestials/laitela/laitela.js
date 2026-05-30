5f54e86a1b3a74f8fbcab4266ec24e0f7f7733a4
ShadowTheHedgehog <Christianthehedgehogtessit@gmail.com>
2026-05-30 12:03:45 -0400

    te

--- src/core/celestials/laitela/laitela.js
+++ src/core/celestials/laitela/laitela.js
@@ -48,9 +48,9 @@ export const Laitela = {
   },
   get matterExtraPurchaseFactor() {
     if ((Pelle.isDoomed && !PelleDestructionUpgrade.continuumBuff.canBeApplied) || player.disablePostReality) return 1;
-    return (Decimal.pow(Decimal.pow(new Decimal(Decimal.log10(Currency.darkMatter.max.add(1))).div(50), 0.4).times(0.5).add(1).times(
+    return Decimal.min(Decimal.pow(Decimal.pow(new Decimal(Decimal.log10(Currency.darkMatter.max.add(1))).div(50), 0.4).times(0.5).add(1).times(
       SingularityMilestone.continuumMult.effectOrDefault(new Decimal(0)).add(1)).times(
-      DualityUpgrade(11).effectOrDefault(1)).times(Hadrons.continuumMultiplier), DualityUpgrade(14).effectOrDefault(1))).toNumber();
+      DualityUpgrade(11).effectOrDefault(1)).times(Hadrons.continuumMultiplier), DualityUpgrade(14).effectOrDefault(1)),DC.E250).toNumber();
   },
   get hadronizes() {
     return this.celestial.hadronizes;
